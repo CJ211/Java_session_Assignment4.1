6dda460c8723c5c5e37dafffc865ce45042ee8c2
CJ211 <shhahjay@gmail.com>
2017-01-08 12:43:36 +0530

    Create ABC.java

---
+++ ABC.java
@@ -0,0 +1,25 @@
+package abc.access;
+ 
+
+public class ABC {
+ 
+	
+	void defaultMethod() {
+		System.out.println("Inside Method with DEFAULT Access Modifier");
+	}
+ 
+	
+	public void publicMethod() {
+		System.out.println("Inside Method with PUBIC Access Modifier");
+	}
+
+	private void privateMethod() {
+		System.out.println("Inside Method with PRIVATE Access Modifier");
+	}
+ 
+	
+	protected void protectedMethod() {
+		System.out.println("Inside Method with PROTECTED Modifier");
+	}
+ 
+}
